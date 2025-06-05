5775ef90a06f39096403c79ee185f7530539f644
Maria Kushnarenko <128240837+NureKushnarenkoMariia@users.noreply.github.com>
2025-06-05 15:07:04 +0300

    Delete app directory

--- app/src/main/java/com/example/lifepaw/ui/notification/NotificationItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lifepaw.ui.notification
-
-data class NotificationItem(
-    val title: String,
-    val date: String,
-    val description: String
-)
--- app/src/main/java/com/example/lifepaw/ui/report/Report.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lifepaw.ui.report
-
-data class Report(
-    val title: String,
-    val date: String,
-    val description: String
-)
